3c47d8fd04dde30376f35934362bd7da495303bc
James Costian <james@jamescostian.com>
2014-11-03 17:10:46 -0600

    Add gulpfile for Mocha and JSHint

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var jshint = require('gulp-jshint')
+var mocha = require('gulp-mocha')
+
+require('./traceur.js')
+
+gulp.task('lint', function () {
+	return gulp.src(['index.js', 'traceur.js', 'gulpfile.js', 'package.json', 'lib/**/*.js', 'test/**/*.js'])
+		.pipe(jshint())
+})
+
+gulp.task('test', function () {
+	return gulp.src(['test/**/*.js'], { read: false })
+		.pipe(mocha({
+			ui: 'bdd'
+		}))
+})
+
+gulp.task('default', ['lint', 'test'])
