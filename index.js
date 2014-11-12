4be963fc6e436e0f3ada5f3ddfe6d9fe9aca7712
James Costian <james@jamescostian.com>
2014-11-12 08:27:33 -0600

    Removing traceur

--- index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var traceur = require('./traceur.js')
-
-module.exports = require('./lib/module.js')
--- test/extend-config.js
+++ test/extend-config.js
@@ -1,31 +1,30 @@
-let assert = require('assert')
-let util = require('util')
-require('../traceur.js')
+var assert = require('assert')
+var util = require('util')
 
-let extend = require('../lib/extend-config.js')
+var extend = require('../lib/extend-config.js')
 
-describe('extend-config', () => {
-	it('should create needed objects if passed undefined', () => {
+describe('extend-config', function () {
+	it('should create needed objects if passed undefined', function () {
 		assert.equal(typeof extend(), typeof {})
 		assert.equal(typeof extend().rethinkdb, typeof {})
 	})
-	it('should create needed objects if passed a blank object', () => {
+	it('should create needed objects if passed a blank object', function () {
 		assert.equal(typeof extend({}), typeof {})
 		assert.equal(typeof extend().rethinkdb, typeof {})
 	})
-	describe('non-blank objects passed to extend-config', () => {
-		let config = extend({rethinkdb: {port: 'hi'}}) // This should overwrite the returned object's rethinkdb.port to "hi"
-		let config2 = extend({rethinkdb2: {port: 'hi'}}) // This should not overwrite anything, but rethinkdb2 should be preserved
-		it('should create needed objects', () => {
+	describe('non-blank objects passed to extend-config', function () {
+		var config = extend({rethinkdb: {port: 'hi'}}) // This should overwrite the returned object's rethinkdb.port to "hi"
+		var config2 = extend({rethinkdb2: {port: 'hi'}}) // This should not overwrite anything, but rethinkdb2 should be preserved
+		it('should create needed objects', function () {
 			assert.equal(typeof config, typeof {}, 'config should be an object')
 			assert.equal(typeof config.rethinkdb, typeof {}, 'config.rethinkdb should be an object')
 			assert.equal(typeof config2, typeof {}, 'config2 should be an object')
 			assert.equal(typeof config2.rethinkdb, typeof {}, 'config2.rethinkdb should be an object')
 		})
-		it('should preserve options it doesn\'t understand', () => {
+		it('should preserve options it doesn\'t understand', function () {
 			assert.equal(typeof config2.rethinkdb2, typeof {})
 		})
-		it('should not overwrite things', () => {
+		it('should not overwrite things', function () {
 			assert.equal(config.rethinkdb.port, 'hi')
 		})
 	})
