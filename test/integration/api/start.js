3f6e1ec30fc5cd2f4ef63eca81edeb0aca0a9100
James Costian <james@jamescostian.com>
2015-01-13 09:09:27 -0600

    New tests for the API additions and import code

--- test/integration/api/start.js
+++ test/integration/api/start.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var thePath = __dirname + '/.deleteme-api'
+require('rimraf').sync(thePath)
 var flashcardz = require('../../../lib/module.js')({path: thePath})
 
 test('API integration', function (t) {
@@ -40,11 +41,11 @@ test('API integration', function (t) {
 	})
 	t.deepEqual(flashcardz.get('myStackOfFlashcards'), expectedModel)
 
-	flashcardz.set('myStackOfFlashcards', 'x', {back: 'g'})
+	flashcardz.setCard('myStackOfFlashcards', 'x', {back: 'g'})
 	expectedModel[3].back = 'g'
 	t.deepEqual(flashcardz.get('myStackOfFlashcards'), expectedModel)
 
-	flashcardz.set('myStackOfFlashcards', 3, {front: 'h'})
+	flashcardz.setCard('myStackOfFlashcards', 3, {front: 'h'})
 	expectedModel[3].front = 'h'
 	t.deepEqual(flashcardz.get('myStackOfFlashcards'), expectedModel)
 
@@ -61,8 +62,14 @@ test('API integration', function (t) {
 	// Since myStackOfFlashcards wasn't saved, one should be able to make a new stack with the same name
 	flashcardz.insert('myStackOfFlashcards', expectedModel)
 	t.deepEqual(flashcardz.get('myStackOfFlashcards'), expectedModel)
+
 	flashcardz.save()
-	t.deepEqual(flashcardz.loadStacks(undefined, ['myStackOfFlashcards']).myStackOfFlashcards, expectedModel)
+	require('rimraf').sync(thePath)
+	flashcardz.save()
+	flashcardz.save()
+
+	t.deepEqual(flashcardz.loadStacks().myStackOfFlashcards, expectedModel)
+	t.deepEqual(flashcardz.loadStacks(true).myStackOfFlashcards, expectedModel)
 
 	t.throws(function () {
 		flashcardz.loadStack('yo')
@@ -74,6 +81,53 @@ test('API integration', function (t) {
 	t.doesNotThrow(function () {
 		flashcardz.save('myStackOfFlashcards')
 	})
+
+	flashcardz.gotRight('myStackOfFlashcards', 'palpable')
+	flashcardz.gotRight('myStackOfFlashcards', 'palpable')
+	flashcardz.gotRight('myStackOfFlashcards', 'palpable')
+
+	flashcardz.gotRight('myStackOfFlashcards', 'ostensible')
+	flashcardz.gotRight('myStackOfFlashcards', 'ostensible')
+	flashcardz.gotWrong('myStackOfFlashcards', 'ostensible')
+
+	flashcardz.gotRight('myStackOfFlashcards', 'diaphanous')
+	flashcardz.gotWrong('myStackOfFlashcards', 'diaphanous')
+	flashcardz.gotWrong('myStackOfFlashcards', 'diaphanous')
+
+	flashcardz.gotRight('myStackOfFlashcards', 3)
+	flashcardz.gotRight('myStackOfFlashcards', 3)
+	flashcardz.gotWrong('myStackOfFlashcards', 3)
+
+	t.equal(flashcardz.easiest('myStackOfFlashcards', 'front'), 'palpable')
+	t.equal(flashcardz.hardest('myStackOfFlashcards', 'front'), 'diaphanous')
+	t.equal(flashcardz.easiest('myStackOfFlashcards', 'front'), 'palpable')
+	t.equal(flashcardz.hardest('myStackOfFlashcards', 'front'), 'diaphanous')
+
+	t.deepEqual(flashcardz.import(__dirname + '/import-me', 'tab/newline'), [
+		{
+			back: 'y',
+			front: 'x',
+			right: 0,
+			wrong: 0
+		},
+		{
+			back: 'def',
+			front: 'abc',
+			right: 4,
+			wrong: 1
+		},
+		{
+			back: 'jkl',
+			front: 'ghi',
+			right: 0,
+			wrong: 0
+		}
+	])
+
+	t.throws(function () {
+		flashcardz.import(__dirname + '/import-me')
+	})
+
 
 	require('rimraf').sync(thePath)
 	t.end()
