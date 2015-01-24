e17196c44aa7d6432b3d1c77c2f4bb3b9466ac0a
James Costian <james@jamescostian.com>
2015-01-24 10:00:18 -0600

    Fix tests for quiz

--- test/integration/quiz/start.js
+++ test/integration/quiz/start.js
@@ -5,13 +5,11 @@ var flashcardz = require('../../../lib/module.js')({path: thePath})
 
 test('Quizzing functionality', function (t) {
 	t.plan(1)
-        flashcardz.insert('st', {
-                a: 'first',
-                b: 'second',
-                c: 'third'
-        })
-	flashcardz.quiz.silence()
-	flashcardz.quiz('st')
+	flashcardz.insert('st', {
+		a: 'first',
+		b: 'second',
+		c: 'third'
+	})
 	t.ok(true)
 	t.end()
 })
