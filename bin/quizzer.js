46a7311a55f0d7f920be9b51e84668ae18bdf3c6
James Costian <james@jamescostian.com>
2015-04-04 10:43:09 -0600

    flash-quiz: accept input from STDIN or a file

---
+++ bin/quizzer.js
@@ -0,0 +1,24 @@
+var f = require('../lib/module.js')
+var fs = require('fs')
+var stack
+module.exports = function (path, cliOpts) {
+	var options = {}
+	var file = fs.readFileSync(path)
+	stack = JSON.parse(file)
+
+	console.log('Each question is the back of a card. Type in what\'s on the front of the card')
+	console.log('When you want to stop, just push ctrl+c and everything will be saved.')
+
+	process.on('exit', function () {
+		fs.writeFileSync(path, JSON.stringify(stack))
+		console.log('Saved progress :)')
+	})
+	startTheQuiz(options)
+}
+
+function startTheQuiz(options) {
+	f.quiz(stack, options).then(function (newStack) {
+		stack = newStack
+		startTheQuiz(options)
+	})
+}
