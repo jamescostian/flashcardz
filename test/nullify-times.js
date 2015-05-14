74058495a411799619e6488ec2d7223fc49b985c
James Costian <james@jamescostian.com>
2015-05-14 13:21:09 -0500

    Add test helper to nullify times

---
+++ test/nullify-times.js
@@ -0,0 +1,20 @@
+module.exports = function (stackOrCard) {
+	if (typeof stackOrCard.front !== 'undefined') {
+		// This is a card
+		return {
+			front: stackOrCard.front,
+			back: stackOrCard.back,
+			history: stackOrCard.history.map(function (event) {
+				return {
+					recalled: event.recalled,
+					time: null
+				}
+			})
+		}
+	}
+	else {
+		// This is a stack of cards, so put each card through this method
+		// console.log(stackOrCard)
+		return stackOrCard.map(module.exports)
+	}
+}
