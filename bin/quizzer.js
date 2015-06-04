7e388520c85f97c2772cd7242e6be36c7e597902
James Costian <james@jamescostian.com>
2015-06-04 08:21:03 -0500

    Make flash-quiz use f.pick.smart

--- bin/quizzer.js
+++ bin/quizzer.js
@@ -1,17 +1,16 @@
 var f = require('../lib/module.js')
 var fs = require('fs')
 var stack
-var lastCardID = Number.MAX_VALUE
 var thePath
 module.exports = function (path, options) {
 	var file = fs.readFileSync(path)
-	stack = JSON.parse(file)
+	stack = f.convert(JSON.parse(file), 'nice')
 	thePath = path
 
 	// Clear the screen
 	process.stdout.write('\x1bc')
 
-	console.log('Each question is the back of a card. Type in what\'s on the front of the card')
+	console.log('Each question is the back of a card. Type in what\'s on the front of the card.')
 	console.log('When you want to stop, just push ctrl+c and everything will be saved.')
 
 	// Don't allow options.show to match options.answer
@@ -25,23 +24,37 @@ module.exports = function (path, options) {
 	startTheQuiz(options)
 }
 
+function pluralize(num, noun, doNotSay1) {
+	if (num === 1 && !!doNotSay1) {
+		return noun
+	}
+	else if (num === 1) {
+		return num + ' ' + noun
+	}
+	else {
+		return num + ' ' + noun + 's'
+	}
+}
+
 function startTheQuiz(options) {
 	// First, save the progress
 	fs.writeFileSync(thePath, JSON.stringify(stack))
 
-	// Pick from a list of cards which excludes the last card
-	var pickFrom = stack.filter(function (card, id) {
-		return id !== lastCardID
-	})
+	var oops = stack.filter(function (card) {
+		if (card.history.length < options.acceptance) {
+			return true
+		}
+		for (var j = card.history.length - 1; j >= card.history.length - options.acceptance; j -= 1) {
+			if (!card.history[j].recalled) {
+				return true
+			}
+		}
+		return false
+	}).reduce(function (count) {return count + 1}, 0)
+	console.log('There ' + (oops === 1 ? 'is' : 'are') + ' ' + pluralize(oops, 'card') + ' which you have not correctly recalled the last ' + pluralize(options.acceptance, 'time', true) + ' or have seen less than ' + pluralize(options.acceptance, 'time'))
 
-	var newCardID = f.pick.evenHardish(pickFrom)
-	if (newCardID >= lastCardID) {
-		newCardID += 1
-	}
-
-	f.quiz(stack[newCardID], f.cliQuizzer(options)).then(function (newCardState) {
-		stack[newCardID] = newCardState
-		lastCardID = newCardID
+	f.quiz(stack, require('../cli-quizzer')(options), f.pick.smart(options)).then(function (newState) {
+		stack = newState
 		startTheQuiz(options)
 	})
 }
